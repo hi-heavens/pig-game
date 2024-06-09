da92076bb2bef9c782716fa1a86cb49ce7104155
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2024-06-09 20:33:20 +0100

    display the winner, hide the dice and stop the game

--- script.js
+++ script.js
@@ -1,56 +1,82 @@
 'use strict';
 
-// Select the elements
-const score0El = document.getElementById('score--0');
-const score1El = document.getElementById('score--1');
-
+// Selecting the elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
 
-// getting the buttons el
+// Selecting the player elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+// Selecting the players score elements
+const currentScore0El = document.getElementById('current--0');
+const currentScore1El = document.getElementById('current--1');
+
+// Selecting the game buttons
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// To hold the current score
-let score = 0;
-
-// Active player
-let active = 0;
-
-// getting the player current score el
-const currentEl = document.getElementById(`current--${active}`);
-const current1El = document.getElementById('current--1');
-
-// Start the game with both players score having zero
+// Initial conditions when the game start
 score0El.textContent = 0;
 score1El.textContent = 0;
-// The dice is invicible at the beginning
 diceEl.classList.add('hidden');
 
-// User rolls a dice
-btnRoll.addEventListener('click', function() {
-    // Generate random dice roll
+let currentScore = 0;
+let activePlayer = 0;
+const scores = [0, 0];
+let stillPlaying = true;
+
+// When a player rolls the dice
+btnRoll.addEventListener('click', function () {
+  if (stillPlaying) {
+    // generate a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
-    diceEl.src = `dice-${dice}.png`;
-    // display dice roll
+    // remove the hidden class from the dice and display the random dice
     diceEl.classList.remove('hidden');
-    if (dice === 1) {
-        // switch the player
-        document.querySelector(`.player--${active}`).classList.remove('player--active');
-        active = active === 0 ? 1 : 0;
-        document.querySelector(`.player--${active}`).classList.toggle('player--active');
-        currentEl.textContent = 0;
+    diceEl.src = `dice-${dice}.png`;
+    // check if rolled dice === 1 or not
+    if (dice !== 1) {
+      // add score to the current player
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
     } else {
-        score += dice;
-        currentEl.textContent = score;
+      // Adjust the current player score === 0
+      document.getElementById(`current--${activePlayer}`).textContent = 0;
+      // switch the active player
+      activePlayer = activePlayer ? 0 : 1;
+      currentScore = 0;
+      player0El.classList.toggle('player--active');
+      player1El.classList.toggle('player--active');
     }
-})
+  }
+});
 
-btnHold.addEventListener('click', function() {
-    score0El.textContent = score;
-    if (score >= 100) {
-        
+btnHold.addEventListener('click', function () {
+  if (stillPlaying) {
+    scores[activePlayer] += currentScore;
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 20) {
+      stillPlaying = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      // document
+      //   .querySelector(`.player--${activePlayer}`)
+      //   .classList.remove("player--active");
+    } else {
+      // Adjust the current player score === 0
+      document.getElementById(`current--${activePlayer}`).textContent = 0;
+      // switch the active player
+      activePlayer = activePlayer ? 0 : 1;
+      currentScore = 0;
+      player0El.classList.toggle('player--active');
+      player1El.classList.toggle('player--active');
     }
-})
-
-
+  }
+});
